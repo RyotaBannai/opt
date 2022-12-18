5708e125473291765173844c4ff3f264c9ff20b3
RyotaBannai <ryotala0528@gmail.com>
2022-12-18 14:28:17 +0900

    add: inferencing probability by quadratic programming

--- src/recommendation.py
+++ src/recommendation.py
@@ -1,12 +1,14 @@
 """
-商品水栓のための興味スコアリング
+商品推薦のための興味スコアリング
+確率推定問題（あるデータをもとに何か確率を求めたい）を凸二次計画問題にモデリングする
+凸二次計画問題: 目的関数が凸な二次関数であり、制約式が線形の不等式で書ける最適化問題
 
 ユーザーの商品閲覧履歴を活用
 
 Recency に関する単調整：ユーザーは最近閲覧した商品ほど興味がある
 Frequency に関する単調整：ユーザーは何度も閲覧した商品ほど興味がある
 
-実務においてアルゴリズムを中心に商品水栓のロジックを考えてはいけない. 
+実務においてアルゴリズムを中心に商品水栓のロジックを考えてはいけない.
 商品推薦は、ユーザー体験を起点に、どのようなユーザーがどのタイミングでどんなニーズがあるのか？を考えることが重要. 238
 """
 
@@ -15,10 +17,10 @@ from datetime import datetime
 from pathlib import Path
 from typing import Dict, List, Tuple
 
+import cvxopt
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import pulp
 from mpl_toolkits.mplot3d import Axes3D
 
 # %%
@@ -131,3 +133,146 @@ X, Y = np.meshgrid(Rec, Freq)  # x,y
 fig = plt.figure()
 ax = fig.add_subplot(111, projection="3d", xlabel="rec", ylabel="freq", zlabel="prob")
 ax.plot_wireframe(X, Y, Z)
+
+# %%
+# 1) rec と freq に対して再閲覧確率 pred_prob を推定する
+R = sorted(tar_df["rec"].unique().tolist())
+F = sorted(tar_df["freq"].unique().tolist())
+
+# cvxopt では辺数ベクトルと積をとる行列やベクトルを直接定義するため、変数を明示的に定義する必要はない.
+# その代わり、数理モデルにおける変数 pred_prob_{r,f} が何番目の変数か索引をつける必要がある.
+Idx = []
+RF2Idx = {}
+index = 0
+for r in R:
+    for f in F:
+        Idx.append(index)
+        RF2Idx[r, f] = index
+        index += 1
+
+G_list = []  # 制約式に現れる変数の係数行列
+h_list = []  # 制約式に現れる定数項のベクトルを作るためのリスト
+# 変数の係数行列を作成するためのデフォルトの変数の係数ベクトル
+# 制約式や、目的関数を定義する際にデフォルトで利用するベクトル
+var_vec = [0.0] * len(Idx)
+
+# pred_prob は確率なので、0以上1以下の値をとる変数
+# -pred_prob[r,f] <=0 の実装.
+for r in R:
+    for f in F:
+        idx = RF2Idx[r, f]
+        G_row = var_vec[:]
+        G_row[idx] = -1  # pred_prob[r,f] の係数は-1 (<=0 としている)
+        G_list.append(G_row)
+        h_list.append(0)  # 右辺の定数項 0
+
+# pred_prob[r,f] <=1 の実装.
+for r in R:
+    for f in F:
+        idx = RF2Idx[r, f]
+        G_row = var_vec[:]
+        G_row[idx] = 1  # pred_prob[r,f] の係数は1
+        G_list.append(G_row)
+        h_list.append(1)  # 右辺の定数項 1
+
+# 2) pred_prob はrec について単調減少する
+# より最近に商品をみた場合の確率の方が、再閲覧確率が高くなることを表現する
+# 3) pred_prob はfreq について単調増加する
+# より頻繁に商品をみた場合の確率の方が、再閲覧確率が高くなることを表現する
+# cvxoptでの制約式 Gx<=h となるよう、右辺に定数項を置く
+
+# -pred_prob[r,f] + pred_prob[r+1,f] <= 0 の実装
+# r+1 しているから、定義域からはみ出さないように注意.
+for r in R[:-1]:
+    for f in F:
+        idx1 = RF2Idx[r, f]
+        idx2 = RF2Idx[r + 1, f]  # 最後に閲覧した日付が１日だけさらに前
+        G_row = var_vec[:]
+        G_row[idx1] = -1  # pred_prob[r,f] の係数は-1
+        G_row[idx2] = 1  # pred_prob[r,f] の係数は1
+        G_list.append(G_row)
+        h_list.append(0)  # 右辺の定数項 0
+
+# pred_prob[r,f] - pred_prob[r,f+1] <= 0 の実装
+for r in R:
+    for f in F[:-1]:
+        idx1 = RF2Idx[r, f]
+        idx2 = RF2Idx[r, f + 1]  # 閲覧回数が１回だけ多い
+        G_row = var_vec[:]
+        G_row[idx1] = 1  # pred_prob[r,f] の係数は1
+        G_row[idx2] = -1  # pred_prob[r,f] の係数は-1
+        G_list.append(G_row)
+        h_list.append(0)  # 右辺の定数項 0
+
+
+# 4) pred_prob と prob の二乗誤差を後見すうの重み付きで最小化する
+
+P_list = []  # 目的関数の変数の２次の項の係数行列を作るためのリスト
+q_list = []  # 目的関数の変数の１次の項の係数ベクトルを作るためのリスト
+
+# N[r,f] * pred_prob[r,f] ^ 2 - 2 *N[r,f] * pred_prob[r,f] の実装
+# pred_prob[r,f] が変数だから、定数項は削除してある.
+
+for r in R:
+    for f in F:
+        idx = RF2Idx[r, f]
+        N = RF2N[r, f]
+        prob = RF2Prob[r, f]  # 実測値
+        P_row = var_vec[:]
+        P_row[idx] = 2 * N  # (1/2) を打ち消すために２をかける.
+        P_list.append(P_row)
+        q_list.append(-2 * N * prob)
+
+
+# 行列の作成
+G = cvxopt.matrix(np.array(G_list), tc="d")
+h = cvxopt.matrix(np.array(h_list), tc="d")
+P = cvxopt.matrix(np.array(P_list), tc="d")
+q = cvxopt.matrix(np.array(q_list), tc="d")
+
+solve = cvxopt.solvers.qp(P, q, G, h)
+status = solve["status"]
+
+# %%
+RF2PredProb = {}
+X = solve["x"]
+for r in R:
+    for f in F:
+        idx = RF2Idx[r, f]
+        pred_prob = X[idx]
+        RF2PredProb[r, f] = pred_prob
+rf_df["pred_prob"] = rf_df.apply(lambda x: RF2PredProb[x["rec"], x["freq"]], axis=1)
+rf_df.head()
+
+# %%
+
+# テーブル形式
+# rf_df.pivot_table(index="rec", columns="freq", values="pred_prob")
+
+# 3d plot
+Freq = rf_df.freq.unique().tolist()
+Rcen = rf_df.rec.unique().tolist()
+Z = [
+    rf_df[(rf_df["freq"] == freq) & (rf_df["rec"] == rcen)]["pred_prob"].iloc[0]
+    for freq in Freq
+    for rcen in Rcen
+]
+Z = np.array(Z).reshape((len(Freq), len(Rcen)))
+X, Y = np.meshgrid(Rcen, Freq)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection="3d", xlabel="rec", ylabel="freq", zlabel="pred_prob")
+ax.plot_wireframe(X, Y, Z)
+
+
+# %%
+# サンプルデータで再閲覧確率を推定
+Rows4 = [
+    ("item1", 1, 6),
+    ("item2", 2, 2),
+    ("item3", 1, 2),
+    ("item4", 1, 1),
+]
+sample_df = pd.DataFrame(Rows4, columns=["item_name", "rec", "freq"])
+pd.merge(sample_df, rf_df, on=["rec", "freq"])
+
+# %%
